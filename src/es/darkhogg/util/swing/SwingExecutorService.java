301ccec3a310a56ca0fdb4de09c8a7f5a19600fd
Daniel Escoz <darkhogg@gmail.com>
2012-02-24 21:54:35 +0100

    Added `SwingExecutorService` class
    * Added class `SwingExecutorService`, an `ExecutorService` which enqueues all tasks on the Swing EDT.

---
+++ src/es/darkhogg/util/swing/SwingExecutorService.java
@@ -0,0 +1,51 @@
+package es.darkhogg.util.swing;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * An <tt>ExecutorService</tt> that executes all tasks in the <i>Swing EDT</i>.
+ * 
+ * @author Daniel Escoz Solana
+ * @version 1.0
+ */
+public final class SwingExecutorService extends AbstractExecutorService {
+	
+	/** Whether this executor is shutdown */
+	private volatile boolean shutdown = false;
+	
+	@Override
+	public void shutdown () {
+		shutdown = true;
+	}
+	
+	@Override
+	public List<Runnable> shutdownNow () {
+		return Collections.emptyList();
+	}
+	
+	@Override
+	public boolean isShutdown () {
+		return shutdown;
+	}
+	
+	@Override
+	public boolean isTerminated () {
+		return shutdown;
+	}
+	
+	@Override
+	public boolean awaitTermination ( final long timeout, final TimeUnit unit ) {
+		return false;
+	}
+	
+	@Override
+	public void execute ( final Runnable command ) {
+		SwingUtilities.invokeLater( command );
+	}
+	
+}
